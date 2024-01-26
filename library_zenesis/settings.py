82a1a4a0796d8d0e650ed7168927995b3f4ad3ec
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2024-01-26 14:26:36 +0600

    DEBUG=False SET

--- library_zenesis/settings.py
+++ library_zenesis/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://library-zenesis.onrender.com", "https://*.127.0.0.1"]
@@ -145,9 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
 MEDIA_URL = '/media/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
